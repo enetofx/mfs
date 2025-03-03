4da153304f5aca874e52285be549fa56ec189951
Ernesto Flores <ernesto.flores@medecision.com>
2025-03-03 13:17:59 -0600

    Add navigation between Container and MF-Marketing

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,12 +1,24 @@
-import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { mount } from 'marketing/MarketingApp';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        // The Container noticed navigation in MF-Marketing
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+    // Whenever some navigation occurs on Container history object is going to call provided function (MF-Marketing function)
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    // Whenever some navigation occurs, history object is going to call provided function (Container function)
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    // Return function to sync Browser URL when navigation occurs outside MF-Application (on the Container)
+    // This sync is not needed if running standalone, since the Browser URL is already in sync with the Browser History object
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname); // Sync History object with browser URL
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation, call mount immediately
@@ -12,7 +32,8 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    // If running standalone, set "Browser History" approach (handle browser URL)
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
